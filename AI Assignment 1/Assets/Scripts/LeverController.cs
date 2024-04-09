dd7689e1fcde6e2948245adcba32c563f391c943
chrisewoi <69186935+chrisewoi@users.noreply.github.com>
2024-04-10 01:48:47 +1000

    Added slimes and built half environment

---
+++ AI Assignment 1/Assets/Scripts/LeverController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverController : MonoBehaviour
+{
+    public bool switchOn = false;
+    [SerializeField] private GameObject Switch;
+    [SerializeField] private GameObject Door;
+    [SerializeField] private GameObject Canvas;
+    public float interactDistance = 2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Canvas.SetActive(false);
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactDistance))
+        {
+            if (hit.collider.transform.IsChildOf(transform))
+            {
+                Canvas.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    switchOn = !switchOn;
+                }
+            }
+        }
+
+
+
+            Quaternion newRotation;
+        if (switchOn)
+        {
+            newRotation = Quaternion.Euler(-60, 0, 0);
+            //Vector3 startPos = new Vector3(Door.transform.position.x, 0f, Door.transform.position.z);
+            Vector3 startPos = Door.transform.position;
+            Vector3 endPos = new Vector3(Door.transform.position.x, -9.6f, Door.transform.position.z);
+            Door.transform.position = Vector3.MoveTowards(startPos, endPos, 3 * Time.deltaTime);
+
+        } else
+        {
+            newRotation = Quaternion.Euler(0, 0, 0);
+
+            Vector3 startPos = Door.transform.position;
+            Vector3 endPos = new Vector3(Door.transform.position.x, 0f, Door.transform.position.z);
+            Door.transform.position = Vector3.MoveTowards(startPos, endPos, 3 * Time.deltaTime);
+        }
+        Switch.transform.rotation = newRotation;
+    }
+}
